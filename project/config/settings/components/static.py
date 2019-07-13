52bce28e6d7c3bd93f50787eb5b454e6e10c74b4
권재원 <springkjw@gmail.com>
2019-07-14 02:41:50 +0900

    배포 몇몇 오류 수정

--- project/config/settings/components/static.py
+++ project/config/settings/components/static.py
@@ -8,7 +8,6 @@ if ENV == 'production':
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'ap-northeast-2'
-elif ENV == 'staging':
-    pass
+    AWS_S3_ADDRESSING_STYLE = "path"
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
